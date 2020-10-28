e5fa89af106fc11292fe3430183e95baf46af600
rrilla <rrilla@naver.com>
2020-10-29 00:09:37 +0900

    android 페이지 구성
    1. 뷰페이저, 바텀네비 연동하여 프래그먼트 구현
    2. 앱바,액션바 커스터마이징
    3. 네비바 추가

---
+++ MatchingApp3/app/src/main/java/com/project/matchingapp3/fragment/Fragment1.java
@@ -0,0 +1,34 @@
+package com.project.matchingapp3.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+import com.project.matchingapp3.MainActivity;
+import com.project.matchingapp3.R;
+
+public class Fragment1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+//        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main, container, false);
+//
+//        Button button = rootView.findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                MainActivity activity = (MainActivity) getActivity();
+//                activity.onFragmentChanged(0);
+//            }
+//        });
+//        return rootView;
+
+        return inflater.inflate(R.layout.fragment1, container, false);
+    }
+
+}
