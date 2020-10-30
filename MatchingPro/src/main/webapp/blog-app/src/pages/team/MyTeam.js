5150f1a0fe42dfd3c3b0d862cae8ffa2b71095c9
narare0551 <naraekang20@gmail.com>
2020-10-30 10:54:12 +0900

    teamcreateDuplicateCheck_but DB does not work

---
+++ MatchingPro/src/main/webapp/blog-app/src/pages/team/MyTeam.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MyTeam = () => {
+	return (
+		<div>
+			my team page
+		</div>
+	);
+};
+// 일단 우리팀의 id를 찾는다
+// fetch로 팀 상세보기해서 우리팀 정보 가져와서 보여준다
+//팀장일시 : 팀 가입 요청  목록 ) partys로 가져움 + 승인 버튼 , 추방 버튼 
+
+// localhost:8000/givemeid 요청을 보내면 그 유저가 어떤 유저인지 파악해서 id를 리턴해주는 걸 만들면 되겠지?
+
+
+export default MyTeam;
