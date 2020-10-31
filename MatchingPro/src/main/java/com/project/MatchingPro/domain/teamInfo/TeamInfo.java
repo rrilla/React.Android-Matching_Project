75c5a18b4194d9ed00d7807dee24202b051fe84a
myeongsu-ko <rhaudtn1234@naver.com>
2020-10-31 12:09:14 +0900

    다햇숨다! 파티아이디받아와서 파티정보

--- MatchingPro/src/main/java/com/project/MatchingPro/domain/teamInfo/TeamInfo.java
+++ MatchingPro/src/main/java/com/project/MatchingPro/domain/teamInfo/TeamInfo.java
@@ -7,20 +7,20 @@ import com.project.MatchingPro.domain.user.User;
 
 import lombok.Data;
 
-@Data
-@Entity
+//@Data
+//@Entity
 public class TeamInfo {
-	
-	private Team team;
-	private User user1;
-	private User user2;
-	private User user3;
-	private User user4;
-	private User user5;
-	private User user6;
-	private User user7;
-	private User user8;
-	private User user9;
-	private User user10;
-	private User user11;
+//	
+//	private Team team;
+//	private User user1;
+//	private User user2;
+//	private User user3;
+//	private User user4;
+//	private User user5;
+//	private User user6;
+//	private User user7;
+//	private User user8;
+//	private User user9;
+//	private User user10;
+//	private User user11;
 }
