27773eb31affcdf6d80a592c8d419da779206d13
hayoun <gkdbsx@naver.com>
2020-11-07 22:56:36 +0900

    saturday final commit

--- MatchingPro/src/main/webapp/blog-app/src/App.js
+++ MatchingPro/src/main/webapp/blog-app/src/App.js
@@ -11,8 +11,6 @@ import Mypage from './pages/user/Mypage';
 import LoginModal from './components/modal/LoginModal';
 
 const App = () => {
-
-
   // 페이지가 로딩 되면 localStorage에서 token을 check해서 true or false를 return해준다
   const tokenCheck = () => {
     console.log("App.js:: display Authorization - 로그인 여부 확인을 위한 token check", localStorage.getItem("Authorization"));
@@ -45,16 +43,16 @@ const App = () => {
       {/* 메인화면 */}
 
       <Route path="/Join" exact={true} component={MainForm}></Route>
-      <Route path="/Login" exact={true} component={MainForm}><LoginModal setLoginId={() => setID()}></LoginModal></Route>
+      <Route path="/Login" exact={true} component={MainForm}></Route>
+      {/* <LoginModal setLoginId={() => setID()}></LoginModal> */}
       {/* 로그인과 회원가입, 둘 다 모달로 처리 */}
 
       <Route path="/Team_detail/:id" exact={true} component={Team_detail}></Route>
-
       <Route path="/Team_schedule/:id" exact={true} component={Team_schedule}></Route>
 
       <Route path="/Team_create" exact={true} component={Team_create}></Route>
-      <Route path="/MyTeam" exact={true} component={MyTeam}><MyTeam loginId={loginId}></MyTeam></Route>
 
+      <Route path="/MyTeam" exact={true} component={MyTeam}><MyTeam loginId={loginId}></MyTeam></Route>
       <Route path="/Mypage" exact={true} component={Mypage}></Route>
 
       {/* path에 적은 주소로 요 이 들어오면 component를 return해준다 */}
