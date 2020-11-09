33a638fe1f21fc2a6bdbd4eac464c7def60862f2
myeongsu-ko <rhaudtn1234@naver.com>
2020-11-09 11:35:15 +0900

    팀이미지추가, 랭크수정

--- MatchingPro/src/main/java/com/project/MatchingPro/domain/score/ScoreRepository.java
+++ MatchingPro/src/main/java/com/project/MatchingPro/domain/score/ScoreRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ScoreRepository extends JpaRepository<Score, Integer> {
 	
-	//<11/05>
-	@Query(value = "select * from score order by total desc", nativeQuery = true)//nativeQuery: 이쿼리를 사용하겠다.
+
+@Query(value = " select team_id, total, id, draw,lose,win, (SELECT count(*)+1 from score where total > t.total ) As rank from score As t order by rank", nativeQuery = true)//nativeQuery: 이쿼리를 사용하겠다.
 	List<Score> rank();
 }
